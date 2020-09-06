ac386f1f2d59277a108cb550ff3806d74e559f6a
IpankajI <kmrpnk@gmail.com>
2020-09-06 06:18:33 +0530

    fixed bug for aho

--- aho_corasick.cpp
+++ aho_corasick.cpp
@@ -1,163 +1,169 @@
-
 #include<bits/stdc++.h>
+
+
 using namespace std;
 
-string s[1000];
-class Aho{
+class Aho
+{
+private:
     struct AhoNode
     {
-        int children[26];
+        map<int, int> children;
+        char ch;
+        int suffixLink;
+        int wordEnd;
         bool leaf;
         int parent;
-        int suffixLink;
         int wordId;
-        char pChar;
-        int endWordLink;
         AhoNode(){
+            ch='.';
+            suffixLink=-1;
+            wordEnd=-1;
             leaf=false;
             parent=-1;
-            suffixLink=-1;
             wordId=-1;
-            pChar='.';
-            endWordLink=-1;
-            memset(children, 0, sizeof(children));
         }
     };
-
+    
     vector<AhoNode> trie;
     vector<int> wordLen;
-    int sz;
     int root;
+    int sz;
 
-public:
-    Aho(){
-        sz=0;
-        root=0;
-        trie.push_back(AhoNode());
-    }
-
-    void addString(string s, int id){
-        int cur=root;
-        int n=s.length();
-
-        for(int i=0;i<n;++i){
-            int child=s[i]-'a';
-            if(trie[cur].children[child]==0){
-                trie.push_back(AhoNode());
-                trie[sz].parent=cur;
-                trie[sz].pChar=s[i];
-                trie[cur].children[child]=sz;
-
-                sz++;
-            }
-
-            cur=trie[cur].children[child];
-        }
-
-        trie[cur].leaf=true;
-        trie[cur].wordId=id;
-        wordLen.push_back(n);
-    }
-    
-    void buildAho(){
-        queue<int> q;
-        q.push(root);
-
-        while(!q.empty()){
-            int cur=q.front(); q.pop();
-            buildSuffix(cur);
-            for(int i=0;i<26;++i){
-                if(trie[cur].children[i]!=0){
-                    q.push(trie[cur].children[i]);
-                }
-            }
-        }
-    }
-
-    void buildSuffix(int node){
+    void buildLink(int node){
         if(node==root){
             trie[node].suffixLink=root;
-            trie[node].endWordLink=root;
+            trie[node].wordEnd=root;
             return;
         }
 
         if(trie[node].parent==root){
             trie[node].suffixLink=root;
             if(trie[node].leaf){
-                trie[node].endWordLink=node;
+                trie[node].wordEnd=node;
             }
             else{
-                trie[node].endWordLink=root;
+                trie[node].wordEnd=trie[trie[node].suffixLink].wordEnd;
             }
             return;
         }
 
-        int cur=trie[trie[node].parent].suffixLink;
-        int child=trie[node].pChar-'a';
+        int next=trie[trie[node].parent].suffixLink;
+        int ch=trie[node].ch;
+
         while(true){
-            if(trie[cur].children[child]!=0){
-                trie[node].suffixLink=trie[cur].children[child];
+            if(trie[next].children.count(ch)){
+                trie[node].suffixLink=trie[next].children[ch];
                 break;
             }
 
-            if(cur==root){
+            if(next==root){
                 trie[node].suffixLink=root;
-                break;                
+                break;
             }
 
-            cur=trie[cur].suffixLink;
+            next=trie[next].suffixLink;
         }
 
         if(trie[node].leaf){
-            trie[node].endWordLink=node;
+            trie[node].wordEnd=node;
         }
         else{
-            trie[node].endWordLink=trie[trie[node].suffixLink].endWordLink;
+            trie[node].wordEnd=trie[trie[node].suffixLink].wordEnd;
         }
-        
+    }
+
+public:
+    Aho(){
+        root=0;
+        trie.push_back(AhoNode());
+        sz=1;
+    }
+
+    void add(string s, int id){
+        int cur=root;
+
+        for(int i=0;i<s.length();++i){
+            int ch=s[i];
+            if(trie[cur].children.count(ch)==0){
+                trie.push_back(AhoNode());
+                trie[sz].ch=s[i];
+                trie[sz].parent=cur;
+                trie[cur].children[ch]=sz;
+                sz++;
+            }
+            cur=trie[cur].children[ch];
+        }
+        trie[cur].leaf=true;
+        trie[cur].wordId=id;
+        wordLen.push_back(s.length());
+    }
+
+    void buildAho(){
+        queue<int> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int node=q.front(); q.pop();
+            buildLink(node);
+            for(map<int, int>::iterator it=trie[node].children.begin(); it!=trie[node].children.end();++it){
+                q.push(it->second);
+            }
+        }
+
     }
 
     void findMatches(string text){
         int cur=root;
 
         for(int i=0;i<text.length();++i){
-            int child=text[i]-'a';
+            int ch=text[i];
+            
             while(true){
-                if(trie[cur].children[child]){
-                    cur=trie[cur].children[child];
+                if(trie[cur].children.count(ch)){
+                    cur=trie[cur].children[ch];
                     break;
                 }
                 if(cur==root){
                     break;
                 }
-
                 cur=trie[cur].suffixLink;
             }
 
-            int node=cur;
+            int found=cur;
+
             while(true){
-                node=trie[node].endWordLink;
-                if(node==root){
+                int wordEnd=trie[found].wordEnd;
+                if(wordEnd==root){
                     break;
                 }
-                int idx=i-wordLen[trie[node].wordId]+1;
-                cout<<idx<<':'<<text.substr(idx, wordLen[trie[node].wordId])<<',';
-                node=trie[node].suffixLink;
+                int len=wordLen[trie[wordEnd].wordId];
+                int idx=i-len+1;
+                cout<<text.substr(idx, len)<<' ';
+                found=trie[found].suffixLink;
+                
             }
+
         }
     }
+
 };
 
+
 int main(){
-    int n;
-    cin>>n;
+    string s[]={"cat", "mat", "example", "random", "cat"};
+
+    int n=sizeof(s)/sizeof(s[0]);
     Aho aho;
     for(int i=0;i<n;++i){
-        cin>>s[i];
-        aho.addString(s[i], i);
+        aho.add(s[i], i);
+        // aho.addString(s[i]);
+        
     }
     aho.buildAho();
-    string text;
-    cin>>text;
+
+    string text="dsadscardsadamatdsecatdfhwgaedhfrandomdaexamplefdsjkfh";
     aho.findMatches(text);
+    //output- mat cat random example
+    
 }
